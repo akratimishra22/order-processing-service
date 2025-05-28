66b60e935db68d2bdc259a6a8ae3a25ceae66b73
akratimishra22 <AKRATI2212MISHRA@GMAIL.COM>
2025-05-28 23:26:45 +0100

    Add authentication and authorization in each api

--- src/main/java/com/gharelu/order_processing_service/controller/OrderController.java
+++ src/main/java/com/gharelu/order_processing_service/controller/OrderController.java
@@ -2,53 +2,89 @@ package com.gharelu.order_processing_service.controller;
 
 import com.gharelu.order_processing_service.model.Orders;
 import com.gharelu.order_processing_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
     private final OrderService service;
+    @Autowired
+    @Qualifier("authServiceWebClient")
+    private final WebClient webClient;
 
-    public OrderController(OrderService service) {
-        this.service = service;
+    private String isTokenValid(String authHeader) {
+        String authResponse = webClient.get()
+                .header("Authorization", authHeader)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return authResponse;
     }
 
     @PostMapping
-    public ResponseEntity<Orders> placeOrder(@RequestBody Orders order) {
-        return ResponseEntity.ok(service.placeOrder(order));
+    public ResponseEntity<Orders> placeOrder(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
+                                             @RequestBody Orders order) {
+        String authResponse = isTokenValid(authHeader);
+        if(authResponse.equals("Valid"))
+            return ResponseEntity.ok(service.placeOrder(order));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Orders> getOrder(@PathVariable Long id) {
-        return service.getOrderById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Orders> getOrder(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
+                                           @PathVariable Long id) {
+        String authResponse = isTokenValid(authHeader);
+        if(authResponse.equals("Valid"))
+            return service.getOrderById(id)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @GetMapping("/customer/{customerId}")
-    public ResponseEntity<List<Orders>> getOrdersByCustomerId(@PathVariable Long customerId) {
-        return ResponseEntity.ok(service.getOrdersByCustomerId(customerId));
+    public ResponseEntity<List<Orders>> getOrdersByCustomerId(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
+                                                              @PathVariable Long customerId) {
+        String authResponse = isTokenValid(authHeader);
+        if(authResponse.equals("Valid"))
+            return ResponseEntity.ok(service.getOrdersByCustomerId(customerId));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @GetMapping
-    public ResponseEntity<List<Orders>> getAllOrders() {
-        return ResponseEntity.ok(service.getAllOrders());
+    public ResponseEntity<List<Orders>> getAllOrders(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+        String authResponse = isTokenValid(authHeader);
+        if(authResponse.equals("Valid"))
+            return ResponseEntity.ok(service.getAllOrders());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @PutMapping("/{id}/cancel")
-    public ResponseEntity<Void> cancelOrder(@PathVariable Long id) {
-        service.cancelOrder(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Void> cancelOrder(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
+                                            @PathVariable Long id) {
+        String authResponse = isTokenValid(authHeader);
+        if(authResponse.equals("Valid")) {
+            service.cancelOrder(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
 
